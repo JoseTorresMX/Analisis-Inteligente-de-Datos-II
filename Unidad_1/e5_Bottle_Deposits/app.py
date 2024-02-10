ddbfc9426d9241a312a269a5025acce7d4155303
JoseTorres <joseangel.tosa@outlook.com>
2024-02-10 16:32:30 -0700

    ReME_e5, OK

--- Unidad_1/e5_Bottle_Deposits/app.py
+++ Unidad_1/e5_Bottle_Deposits/app.py
@@ -1,12 +1,3 @@
-"""En muchas jurisdicciones se añade un pequeño depósito a los envases de bebidas para animar a la gente a reciclarlos.
-a reciclarlos. En una jurisdicción concreta, los envases de bebidas de un litro o menos tienen un depósito de 0,10
-litro o menos tienen un depósito de 0,10 dólares, y los de más de un litro tienen un depósito de 0,25 dólares.
-0,25 $ de depósito.
-Escribe un programa que lea el número de envases de cada tamaño del usuario.
-Su programa debe continuar calculando y mostrando el reembolso que se recibirá por devolver esos envases.
-por devolver esos envases. Formatee la salida de modo que incluya un signo
-y muestre siempre exactamente dos decimales."""
-
 menos_deposito = 0.10
 mas_deposito = 0.25
 
